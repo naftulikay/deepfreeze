20123c99c2a1eec3b2f1ddfd88b6fa3e670c38df
Naftuli Tzvi Kay <rfkrocktk@gmail.com>
2013-12-05 18:17:23 -0800

    Initial commit of deepfreeze.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "deepfreeze",
+    version = "0.0.1-SNAPSHOT",
+    packages = find_packages('src'),
+    package_dir = { '': 'src'},
+    install_requires = ['setuptools',
+        'boto >= 2.19.0, < 3.0.0',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'deepfreeze = deepfreeze:cli',
+        ]
+    },
+)
